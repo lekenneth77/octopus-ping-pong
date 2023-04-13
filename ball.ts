dcde32ecdea7e24e628c894d8db7b19f260542de
Kenneth Le <kennele@cs.utexas.edu>
2023-04-13 16:37:55 -0500

    FlyControls

--- ball.ts
+++ ball.ts
@@ -4,17 +4,14 @@ const GRAVITY: number = -245.25;
 const DRAG: number = 0.005;
 export class Ball {
     private sphere;
-    // private pos: THREE.Vector3;
     private vel: THREE.Vector3;
-    private clock: THREE.Clock;
     constructor() {
-        const geometry = new THREE.SphereGeometry(1);
+        const geometry = new THREE.SphereGeometry(0.5);
         const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
         const sphere = new THREE.Mesh( geometry, material );
         this.sphere = sphere;
         sphere.castShadow = true;
         this.reset();
-        this.clock = new THREE.Clock();
     }
     
     public reset() {
@@ -22,18 +19,17 @@ export class Ball {
         this.sphere.position.y = 20;
         this.sphere.position.z = -34;   
         this.vel = new THREE.Vector3();
-        this.vel.z = 120;
-        this.vel.x = 40;
+        this.vel.z = 40;
+        // this.vel.x = 40;
     }
 
     public getBall() {
         return this.sphere;
     }
 
-    public update() {
-        let t: number = this.clock.getDelta();
+    public update(t) {
         //TODO: check dynamic t/frame
-        // let t: number = 0.02;
+        //  t = 0.005;
 
         // // update position according to velocity
         if (this.sphere.position.y <= 4 && Math.abs(this.sphere.position.z) <= 1.2 && Math.abs(this.sphere.position.x) < 19.5) {
@@ -49,9 +45,7 @@ export class Ball {
         if (Math.abs(this.sphere.position.z) >= 34.5 && Math.abs(this.sphere.position.x) < 19.5) { 
             this.vel.z = this.vel.z * -1;
             this.vel.x = this.vel.x * -1;
-            // this.vel.y = 40;
-
-            //scale by speed in z and height
+            this.vel.y = 40;
         }
         if(this.sphere.position.y <= 0.5 && Math.abs(this.sphere.position.x) < 19.5) {
             this.vel.y = Math.abs(this.vel.y) - 1;
@@ -65,9 +59,9 @@ export class Ball {
             // Velocity combines gravity and drag acceleration
             let drag = new THREE.Vector3();
             drag.copy(this.vel).multiplyScalar(-1 * DRAG);
-            this.vel.x = drag.x + this.vel.x;
+            // this.vel.x = drag.x + this.vel.x;
             this.vel.y = GRAVITY * t + drag.y + this.vel.y;
-            this.vel.z = drag.z + this.vel.z;
+            // this.vel.z = drag.z + this.vel.z;
         }
 
         this.sphere.position.x = this.vel.x * t + this.sphere.position.x;
--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 import * as THREE from 'three';
 import { Ball } from '/ball';
 import { Vector3 } from 'three';
+import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
 // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const clock = new THREE.Clock();
+
+//first person camera
+const controls = new FlyControls(camera, renderer.domElement);
+const CAM_MOVE_SPD = 100;
+const CAM_ROLL_SPD = Math.PI / 4;
+controls.movementSpeed = CAM_MOVE_SPD;
+controls.domElement = renderer.domElement;
+controls.rollSpeed = CAM_ROLL_SPD;
+controls.autoForward = false;
+controls.dragToLook = true;
+let cam_on = false;
+
+
 let ball = new Ball();
 scene.add( ball.getBall() );
-
 
 //plane
 const planeGeo = new THREE.PlaneGeometry( 39, 69 );
@@ -57,13 +70,17 @@ light.shadow.camera.far = 500; // default
 
 //camera position
 camera.position.z = -30;
-camera.position.x = 30;
+camera.position.x = 60;
 camera.position.y = 20;
 camera.lookAt(0, 0, 0);
 
 function animate() {
 	requestAnimationFrame( animate );
-	ball.update();
+	const deltaT = clock.getDelta();
+	ball.update(deltaT);
+	if (cam_on) {
+		controls.update(deltaT);
+	}
 	// camera.lookAt(ball.sphere.position);
 	renderer.render( scene, camera );
 }
@@ -71,8 +88,13 @@ animate();
 
 addEventListener("keydown", (event) => {
 	switch(event.key) {
+		//Reset Round
 		case('p'):
 			ball.reset();
+			break;
+		//Turn Fly Camera On/Off
+		case('o'):
+			cam_on = !cam_on;
 			break;
 		default:
 			break;
