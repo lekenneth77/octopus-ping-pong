0408795707fd091a878d8189d35a19b2ab80c63b
goldinvo <goldinbaokimvo@gmail.com>
2023-04-20 14:29:39 -0500

    more collisions with verlet

--- ball.ts
+++ ball.ts
@@ -74,16 +74,40 @@ export class Ball {
             this.acc.multiplyScalar(dt*dt*0.5) // + acc*(0.5 * dt^2)
         )
 
+        
 
-        // collisions
+        // collisions  //TODO add y range?
         if (this.sphere.position.y - 2 <= CONST.BALL_RAD && Math.abs(this.sphere.position.x) <= CONST.TABLE_W / 2 && Math.abs(this.sphere.position.z) <= CONST.TABLE_L / 2) {
-            // table collision
-            new_acc = new THREE.Vector3()
-            new_vel = new THREE.Vector3(this.vel.x, Math.abs(this.vel.y), this.vel.z); // lose energy?
-            // spin?
- //       } else if (Math.abs(this.sphere.position.z) >= CONST.TABLE_L / 2 && Math.abs(this.sphere.position.x) < CONST.TABLE_W / 2) {
-            // boundary collision
-        } 
+            // table collision TODO
+            new_vel = new THREE.Vector3(this.vel.x, Math.abs(this.vel.y) - 2, this.vel.z); // lose energy?
+            // spin? TODO
+
+                    //     let dir: THREE.Vector3 = new THREE.Vector3(this.spinAxis.z, 0, -1 * this.spinAxis.x);
+                //     dir = dir.normalize().multiplyScalar( this.spinStrength * .8);
+                //     this.vel.addVectors(this.vel, dir);
+                    
+                //   this.spinStrength *= .8;
+                //     this.hitSide++;
+                //     return;
+                // } 
+         } else if (Math.abs(this.sphere.position.z) >= CONST.TABLE_L / 2 && Math.abs(this.sphere.position.x) < CONST.TABLE_W / 2) {
+            // table boundary
+            new_vel.z = this.getSide() - 1 ? -1 * Math.abs(this.vel.z) : Math.abs(this.vel.z);
+            
+            new_vel.y = 40;
+
+        } else if (this.sphere.position.y <= CONST.NET_H && Math.abs(this.sphere.position.z) <= CONST.BALL_RAD * 2 && Math.abs(this.sphere.position.x) < CONST.TABLE_W / 2) {            // 
+            // net collision
+            //new_vel = new THREE.Vector3(0, this.vel.y * .4, 0)
+        } //else if (this.sphere.position.y <= CONST.FLOOR_Y_POS) {
+        //     //if below table, bam apply absorption from cool pingpong club floor
+        //     new_vel.y = Math.abs(this.vel.y);
+        //     new_vel.y *= CONST.FLOOR_Y_ABSORB;
+        //     new_vel.z *= CONST.FLOOR_XZ_ABSORB; 
+        //     new_vel.x *= CONST.FLOOR_XZ_ABSORB;
+
+        //     new_pos = new_vel.clone().multiplyScalar(dt).add(new_acc.multiplyScalar(dt*dt*0.5));
+        // } 
 
         // set values
         this.acc = new_acc;  
@@ -91,43 +115,6 @@ export class Ball {
         this.sphere.position.x = new_pos.x;
         this.sphere.position.y = new_pos.y;
         this.sphere.position.z = new_pos.z;
-        
-
-        // Update velocity 
-        // Conditions: Net collision, table boundary, table hit, no collision
-
-        // //net collision TOD
-        // if (this.spher(this.sphere.position.y <= CONST.NET_H && Math.abs(this.sphere.position.z) <= CONST.BALL_RAD * 2 && Math.abs(this.sphere.position.x) < CONST.TABLE_W / 2)is.sphere.position.z) >= CONST.TABLE_L / 2 && Math.abs(this.sphere.position.x) < CONST.TABLE_W / 2) { 
-        //     this.vel.z = this.vel.z * -1;
-        //     this.vel.x = this.vel.x * -1;
-        //     this.vel.y = 40;
-        //     return;
-        // }
-
-
-        // //if hit the table (within table bounds) //TODO add y range?
-        // if(this.sphere.position.y <= CONST.BALL_RAD && Math.abs(this.sphere.position.x) <= CONST.TABLE_W / 2 && Math.abs(this.sphere.position.z) <= CONST.TABLE_L / 2) {
-        //     this.vel.y = Math.abs(this.vel.y) - 1;
-        //     // we have the original velocity of the ball
-        //     // project the axis of rotation
-        //     let dir: THREE.Vector3 = new THREE.Vector3(this.spinAxis.z, 0, -1 * this.spinAxis.x);
-        //     dir = dir.normalize().multiplyScalar( this.spinStrength * .8);
-        //     this.vel.addVectors(this.vel, dir);
-            
-        //   this.spinStrength *= .8;
-        //     this.hitSide++;
-        //     return;
-        // } 
-         
-        // // hit the floor
-        // if (this.sphere.position.y <= CONST.FLOOR_Y_POS) {
-        //     //if below table, bam apply absorption from cool pingpong club floor
-        //     this.vel.y = Math.abs(this.vel.y);
-        //     this.vel.y *= CONST.FLOOR_Y_ABSORB;
-        //     this.vel.z *= CONST.FLOOR_XZ_ABSORB; else if () {
-        // }
-        //} 
-        
     }
 
 
